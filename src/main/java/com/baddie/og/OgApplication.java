bbff24e5213d48632dda2f067ac1cbf382d18363
Thangatamil <thangatamil1177@gmail.com>
2025-11-11 08:03:38 +0530

    batman

---
+++ src/main/java/com/baddie/og/OgApplication.java
@@ -0,0 +1,13 @@
+package com.baddie.og;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgApplication.class, args);
+	}
+
+}
